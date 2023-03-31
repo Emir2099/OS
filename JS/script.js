dfd1deafa60d668d37a8f7d6f675a7365cd98fba
Emir || 2099 <emirsteveroger@gmail.com>
2023-03-31 14:47:43 +0530

    refining

--- JS/script.js
+++ JS/script.js
@@ -5,18 +5,21 @@ let searchesicon = document.getElementsByClassName("searchesicon")[0]  // return
 let searchmenu = document.getElementsByClassName("searchmenu")[0]
 
 let vscicon = document.getElementsByClassName("vscicon")[0]  // returns full html collection so we want its first elemnt
-// var vsc_app = new ActiveXObject("WScript.shell") 
+let vscimage = document.getElementsByClassName("vscimage")[0]
 
 windowsicon.addEventListener("click", ()=>{
     if(startmenu.style.bottom == "50px"){
         startmenu.style.bottom = "-700px"
+        windowsicon.style.opacity = "1"
     }
     else{
         startmenu.style.bottom = "50px"
+        windowsicon.style.opacity = "0.8"
     }
 
     if(searchmenu.style.bottom == "50px"){
         searchmenu.style.bottom = "-700px"
+        searchesicon.style.opacity = "1"
     }
     else{
         searchmenu.style.bottom = "-700px"
@@ -29,33 +32,49 @@ windowsicon.addEventListener("click", ()=>{
 searchesicon.addEventListener("click", ()=>{
     if(searchmenu.style.bottom == "50px"){
         searchmenu.style.bottom = "-700px"
+        searchesicon.style.opacity = "1"
     }
     else{
         searchmenu.style.bottom = "50px"
+        searchesicon.style.opacity = "0.8"
     }
 
     if(startmenu.style.bottom == "50px"){
         startmenu.style.bottom = "-700px"
+       windowsicon.style.opacity = "1"
     }
     else{
         startmenu.style.bottom = "-700px"
     }
+
+    
 })
+
 
 
 
 vscicon.addEventListener("click", ()=>{
     if(searchmenu.style.bottom == "50px"){
         searchmenu.style.bottom = "-700px"
+        searchesicon.style.opacity = "1"
     }
     else{
         searchmenu.style.bottom = "-700px"
     }
     if(startmenu.style.bottom == "50px"){
         startmenu.style.bottom = "-700px"
+        windowsicon.style.opacity = "1"
     }
     else{
         startmenu.style.bottom = "-700px"
     }
-    window.alert("Open manually!")
+
+    if(vscimage.style.bottom == "150px"){
+        vscimage.style.bottom = "-700px"
+        vscicon.style.opacity = "1"
+    }
+    else{
+        vscimage.style.bottom = "150px"
+        vscicon.style.opacity = "0.8"
+    }
 })
