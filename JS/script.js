b9477168b2ad8c5220dfcbf908e6d184e27f264e
Emir || 2099 <emirsteveroger@gmail.com>
2023-03-31 14:10:51 +0530

    adding more items

--- JS/script.js
+++ JS/script.js
@@ -4,6 +4,9 @@ let startmenu = document.getElementsByClassName("startmenu")[0]
 let searchesicon = document.getElementsByClassName("searchesicon")[0]  // returns full html collection so we want its first elemnt
 let searchmenu = document.getElementsByClassName("searchmenu")[0]
 
+let vscicon = document.getElementsByClassName("vscicon")[0]  // returns full html collection so we want its first elemnt
+// var vsc_app = new ActiveXObject("WScript.shell") 
+
 windowsicon.addEventListener("click", ()=>{
     if(startmenu.style.bottom == "50px"){
         startmenu.style.bottom = "-700px"
@@ -37,4 +40,22 @@ searchesicon.addEventListener("click", ()=>{
     else{
         startmenu.style.bottom = "-700px"
     }
+})
+
+
+
+vscicon.addEventListener("click", ()=>{
+    if(searchmenu.style.bottom == "50px"){
+        searchmenu.style.bottom = "-700px"
+    }
+    else{
+        searchmenu.style.bottom = "-700px"
+    }
+    if(startmenu.style.bottom == "50px"){
+        startmenu.style.bottom = "-700px"
+    }
+    else{
+        startmenu.style.bottom = "-700px"
+    }
+    window.alert("Open manually!")
 })
